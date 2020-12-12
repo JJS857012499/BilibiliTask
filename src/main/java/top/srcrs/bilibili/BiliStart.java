73f13ccc9a87afbd6c40e0262e5729ac8fc17f7c
jiangjunsheng <jiangjunsheng@baibu.la>
2020-12-12 23:00:45 +0800

    去除校验

--- src/main/java/top/srcrs/bilibili/BiliStart.java
+++ src/main/java/top/srcrs/bilibili/BiliStart.java
@@ -84,24 +84,24 @@ public class BiliStart {
      */
     public static boolean check(){
         JSONObject jsonObject = Request.get("https://api.bilibili.com/x/web-interface/nav");
-        JSONObject object = jsonObject.getJSONObject("data");
-        if("0".equals(jsonObject.getString("code"))){
-            /** 用户名 */
-            data.setUname(object.getString("uname"));
-            /** 账户的uid */
-            data.setMid(object.getString("mid"));
-            /** vip类型 */
-            data.setVipType(object.getString("vipType"));
-            /** 硬币数 */
-            data.setMoney(object.getString("money"));
-            /** 经验 */
-            data.setCurrentExp(object.getJSONObject("level_info").getString("current_exp"));
-            /** 大会员状态 */
-            data.setVipStatus(object.getString("vipStatus"));
-            /** 钱包B币卷余额 */
-            data.setCoupon_balance(object.getJSONObject("wallet").getString("coupon_balance"));
-            return true;
-        }
-        return false;
+//        JSONObject object = jsonObject.getJSONObject("data");
+//        if("0".equals(jsonObject.getString("code"))){
+//            /** 用户名 */
+//            data.setUname(object.getString("uname"));
+//            /** 账户的uid */
+//            data.setMid(object.getString("mid"));
+//            /** vip类型 */
+//            data.setVipType(object.getString("vipType"));
+//            /** 硬币数 */
+//            data.setMoney(object.getString("money"));
+//            /** 经验 */
+//            data.setCurrentExp(object.getJSONObject("level_info").getString("current_exp"));
+//            /** 大会员状态 */
+//            data.setVipStatus(object.getString("vipStatus"));
+//            /** 钱包B币卷余额 */
+//            data.setCoupon_balance(object.getJSONObject("wallet").getString("coupon_balance"));
+//            return true;
+//        }
+        return true;
     }
 }
